39cd210e3ba226d3b34c4ec9a7348b31e854b9a8
Olga Ivzhichenko <olga.ivzhichenko@kaltura.com>
2022-01-04 14:48:54 +0100

    Flower shop tests added

--- src/test/java/com/npo/app/FlowerShopTests.java
+++ src/test/java/com/npo/app/FlowerShopTests.java
@@ -30,7 +30,7 @@ public class FlowerShopTests {
 
     @Test
     //Проверить что пользователь может залогинется
-    public void loginTest() {
+    public void _01_loginTest() {
         //Нажать кнопку войти
         $(".et-menu-account-btn").click();
         //Ввести имя пользователя и пароль
@@ -45,13 +45,13 @@ public class FlowerShopTests {
 
     @Test
     //Проверить что пользователь может добавить товар в избранное
-    public void likeTest() {
+    public void _02_likeTest() {
         //Открыть Магазин -> Новогодний и Рождественский декор
         $("#menu-main #menu-item-4897").hover();
         $("#menu-item-8187").click();
         $(".hero-header-container h1").shouldHave(text("Новогодний и Рождественский декор"));
         //Нажать на кнопку "Добавить в избранное"
-        likeItem(0);
+        likeItem(1);
         //Проверить что счетчик избранного изменился
         $(".et-wishlist-counter").shouldHave(text("1"));
         //Проверить товар в избранном
@@ -62,11 +62,11 @@ public class FlowerShopTests {
 
     @Test
     //Проверить что пользователь может добавить товар в корзину
-    public void addItemToBasketTest() {
+    public void _03_addItemToBasketTest() {
         //Открыть Магазин -> Букеты -> Букеты авторские
-        $("#menu-main #menu-item-4897").hover();
-        $("#menu-item-4926").hover();
-        $("#menu-item-5035").click();
+        $("#menu-main #menu-item-4897").click();
+        $(By.linkText("Букеты")).click();
+        $(By.linkText("Букеты Авторские")).click();
         $(".hero-header-container h1").shouldHave(text("Букеты Авторские"));
         //Нажать на кнопку "В корзину"
         addItem(0);
@@ -81,36 +81,41 @@ public class FlowerShopTests {
 
     @Test
     //Проверить что пользователь может удалить товар из избранного
-    public void deleteLikeTest() {
+    public void _04_deleteLikeTest() {
         //Открыть Магазин -> Новогодний и Рождественский декор
         $("#menu-main #menu-item-4897").hover();
         $("#menu-item-8187").click();
         $(".hero-header-container h1").shouldHave(text("Новогодний и Рождественский декор"));
         //Нажать на кнопку "Добавить в избранное"
-        likeItem(0);
+        likeItem(2);
         //Проверить что счетчик избранного изменился
         $(".et-wishlist-counter").shouldHave(text("1"));
         //Открыть список избранного
         $(".quick_wishlist").click();
+        //Нажать на кнопку удалить возле товара
         $$(".wishlist-items-wrapper tr .remove_from_wishlist").get(0).click();
+        //Проверить, что отображается сообщение Продукт успешно удален из избранного
         $(".woocommerce-message").shouldHave(text("Продукт успешно удален из избранного"));
     }
 
     @Test
-    public void deleteItemFromBasketTest() {
+    public void _05_deleteItemFromBasketTest() {
         //Открыть Магазин -> Букеты -> Букеты авторские
-        $("#menu-main #menu-item-4897").hover();
-        $("#menu-item-4926").hover();
-        $("#menu-item-5035").click();
+        $("#menu-main #menu-item-4897").click();
+        $(By.linkText("Букеты")).click();
+        $(By.linkText("Букеты Авторские")).click();
         $(".hero-header-container h1").shouldHave(text("Букеты Авторские"));
         //Нажать на кнопку "В корзину"
         addItem(0);
         //Проверить что счетчик корзины изменился
         $(".minicart-counter").shouldHave(text("1"));
-        //Проверить товар в корзине
+        //Открыть корзину
         $(".quick_cart").click();
+        //Нажать на кнопку удалить возле товара в корзине
         $$(".woocommerce-mini-cart-item .remove_from_cart_button").get(0).click();
+        //Проверить, что в корзине не отображаются товары
         $$(".woocommerce-mini-cart-item").shouldHave(CollectionCondition.size(0));
+        //Проверить, что отображается сообщение Нет товаров в корзине.
         $(".woocommerce-mini-cart__empty-message").shouldHave(text("Нет товаров в корзине."));
     }
 }
